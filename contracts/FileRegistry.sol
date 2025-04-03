b1296d91e51ddbae9bcbf5ff8e3bf2c313211fb4
Gessica Azevedo <gessicamendonca.a@gmail.com>
2025-04-02 22:35:11 -0300

    adds link field to the FileRegistry contract

--- contracts/FileRegistry.sol
+++ contracts/FileRegistry.sol
@@ -5,36 +5,38 @@ contract FileRegistry {
     struct FileRecord {
         address uploader;
         uint256 timestamp;
+        string link;
     }
 
-    // Mapeia um hash para os dados de quem registrou
     mapping(bytes32 => FileRecord) public files;
 
-    event FileRegistered(bytes32 hash, address uploader, uint256 timestamp);
+    event FileRegistered(bytes32 hash, address uploader, uint256 timestamp, string link);
 
-    function registerFileHash(bytes32 fileHash) public {
+    function registerFileHash(bytes32 fileHash, string memory link) public {
         require(fileHash != bytes32(0), "Invalid Hash");
-        require(files[fileHash].timestamp == 0, "This file has been already registered");
+        require(files[fileHash].timestamp == 0, "This file has already been registered");
 
         files[fileHash] = FileRecord({
             uploader: msg.sender,
-            timestamp: block.timestamp
+            timestamp: block.timestamp,
+            link: link
         });
 
-        emit FileRegistered(fileHash, msg.sender, block.timestamp);
-    }
-
-    function isFileRegistered(bytes32 fileHash) public view returns (bool) {
-        return files[fileHash].timestamp != 0;
+        emit FileRegistered(fileHash, msg.sender, block.timestamp, link);
     }
 
     function getFileUploader(bytes32 fileHash) public view returns (address) {
-        require(isFileRegistered(fileHash), "File not registered");
+        require(files[fileHash].timestamp != 0, "File not registered");
         return files[fileHash].uploader;
     }
 
     function getFileTimestamp(bytes32 fileHash) public view returns (uint256) {
-        require(isFileRegistered(fileHash), "File not registered");
+        require(files[fileHash].timestamp != 0, "File not registered");
         return files[fileHash].timestamp;
+    }
+
+    function getFileLink(bytes32 fileHash) public view returns (string memory) {
+        require(files[fileHash].timestamp != 0, "File not registered");
+        return files[fileHash].link;
     }
 }
